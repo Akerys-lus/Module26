056beea57603c6d5984e88bffd24dec3e74cd1bb
Youranys <yura.lazarev99@mail.ru>
2021-06-01 22:36:11 +0300

    Задача №4.

--- Module14/04_reverse_num/main.py
+++ Module14/04_reverse_num/main.py
@@ -1,3 +1,12 @@
-# TODO здесь писать код
+first_number = input('Введите число: ')
+second_number = input('Введите число: ')
 
+def vice_num(num):
+    vice = ''
+    for sym in num:
+        if sym == '.':
+            vice = vice + sym
+        else:
+            vice = sym + vice
 
+summ_num = float(vice_num(first_number) + vice_num(second_number))
