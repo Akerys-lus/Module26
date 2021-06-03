28dac149598149b355f935ee0158c5c06abbdebe
Youranys <yura.lazarev99@mail.ru>
2021-06-03 23:29:31 +0300

    Задача №4.

--- Module14/04_reverse_num/main.py
+++ Module14/04_reverse_num/main.py
@@ -1,12 +1,29 @@
-first_number = input('Введите число: ')
-second_number = input('Введите число: ')
+# first_number = input('Введите число: ')
+# second_number = input('Введите число: ')
+num = float(input(''))
+
 
 def vice_num(num):
-    vice = ''
+    num = str(num)
+    whole_part = int(num)
+    vice_head = ''
+    vice_end = ''
+    num_end = float(num) - whole_part
+    while int(num_end) / float(num) != 1:
+        num_end *= 10
+    num_end = str(num_end)
     for sym in num:
         if sym == '.':
-            vice = vice + sym
-        else:
-            vice = sym + vice
+            vice_head = vice_head + sym
+        vice_head = sym + vice_head
 
-summ_num = float(vice_num(first_number) + vice_num(second_number))
+    for sym in num_end:
+        vice_end = sym + vice_end
+    vice = vice_head + vice_end
+    print(vice)
+    return vice
+
+
+# summ_num = float(vice_num(first_number)) + float(vice_num(second_number))
+vice_num(num)
+# print(summ_num)
