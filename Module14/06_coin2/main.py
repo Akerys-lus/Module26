75b51ef6b4faf1fdeca43b54806ae307b0718f74
Youranys <yura.lazarev99@mail.ru>
2021-06-04 20:31:43 +0300

    Задача №6.

--- Module14/06_coin2/main.py
+++ Module14/06_coin2/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+import math
+
+print("Введите координаты точки и радиус круга")
+x = float(input("Введите х: "))
+y = float(input("Введите y: "))
+r_circle = float(input("Введите радиус: "))
+
+hypotenuse = math.sqrt(x ** 2 + y ** 2)
+
+if hypotenuse <= r_circle:
+    print("Монетка где-то рядом.")
+else:
+    print("Монетки нет поблизости.")
