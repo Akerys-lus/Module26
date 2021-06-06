d97e7f0e1dce376c6ebaa9a8c5efa7b5d6ff8377
Youranys <yura.lazarev99@mail.ru>
2021-06-06 12:19:17 +0300

    Задача №7.

--- Module14/07_years/main.py
+++ Module14/07_years/main.py
@@ -1,5 +1,5 @@
-a = int(input(''))
-b = int(input())
+a = int(input('Введите первое число: '))
+b = int(input('Введите второе число: '))
 while a < b:
     for i in range(4):
         if 3 == str(a).count(str(i)):
