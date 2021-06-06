6d02af87caf2485f50ef1d1ad9c3258c0265ee89
Youranys <yura.lazarev99@mail.ru>
2021-06-06 12:19:04 +0300

    Задача №5.

--- Module14/05_least_divisor/main.py
+++ Module14/05_least_divisor/main.py
@@ -1,7 +1,7 @@
 n = int(input(''))
 i = 1
 while i <= n:
-  i = i + 1
-  if n % i == 0:
-    print('Наименьший делитель числа', n, 'это:', i)
-    break
+    i = i + 1
+    if n % i == 0:
+        print('Наименьший делитель числа', n, 'это:', i)
+        break
