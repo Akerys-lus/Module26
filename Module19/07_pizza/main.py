32641598218c02f1a481538ea3498b656a7b84c6
Youranys <yura.lazarev99@mail.ru>
2022-01-04 21:40:53 +0300

    19Module, corrected.

--- Module19/07_pizza/main.py
+++ Module19/07_pizza/main.py
@@ -3,13 +3,13 @@ all_orders = {}
 for i in range(1, int(input('Введите кол-во заказов: ')) + 1):
     (surname, order, amount) = input('{} заказ: '.format(i)).split()
 
-    if order in str(all_orders):
+    if order not in str(all_orders):
+
+        all_orders.setdefault(surname, {}).update({order: int(amount)})
+    else:
         all_orders[surname][order] += int(amount)
 
-    else:
-        all_orders.setdefault(surname, {}).update({order: int(amount)})
 
-    print(all_orders)
 for name in all_orders:
     print(name + ':')
     for pizza in all_orders[name]:
