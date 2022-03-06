a3ebd13620d4c8f2c5c52d742d2e9973556d6674
avis <i@email.com>
2022-03-06 16:44:39 +0300

    урок 23, есть недочеты

--- Module23/01_names_2/main.py
+++ Module23/01_names_2/main.py
@@ -9,7 +9,7 @@ for i_len in file_txt:
         if i_symb.isalpha():
             count_symb_len += 1
     try:
-        if count_symb_len > 3:
+        if count_symb_len > 3:  # TODO не верное условие, истинно для имён от 4х символов, а надо от 3х
             sum_symb += count_symb_len
         else:
             sum_symb += count_symb_len
