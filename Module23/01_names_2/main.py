5685eb306ee9c64fc04494ba35ae234d192b5e5f
avis <i@email.com>
2022-03-07 13:50:10 +0300

    урок 23, есть недочеты

--- Module23/01_names_2/main.py
+++ Module23/01_names_2/main.py
@@ -5,12 +5,17 @@ count_len = 0
 for i_len in file_txt:
     count_len += 1
     count_symb_len = 0
-    for i_symb in i_len:
+    for i_symb in i_len:  # TODO количество знаков в имени считайте с помощью len(), но перед этим выполните strip()
+                          #  чтобы убрать символы форматирования
         if i_symb.isalpha():
             count_symb_len += 1
     try:
         if count_symb_len > 4:
-            sum_symb += count_symb_len
+            # TODO Сравнение должно быть такое: if count_symb_len < 3: это логичино и точнее, чем сейчас - отбираются
+            #  имена из 4х буквы бракуются, а должны "браковаться" только имена длиной 0, 1 и 2 символа, из трех букв
+            #  уже "хорошо", согласно заданию
+            sum_symb += count_symb_len  # TODO Эта строка кода дублируется в обоих ветках условного оператора и должна
+                                        #  выполняться безусловно
         else:
             sum_symb += count_symb_len
             raise ValueError
--- Module23/04_registration/main.py
+++ Module23/04_registration/main.py
@@ -11,9 +11,9 @@ def check(line):
     symbols = ('@', '.')
     age = int(age)
     if name.isalpha() is False:
-        raise NotNameError
+        raise NotNameError('Ошибка в имени!')  # TODO вот так указываем поясняющее сообщение...
     elif age not in range(10, 100):
-        raise ValueError()
+        raise ValueError()  # TODO ...добавьте аналогично сюда и четырьмя строками ниже
     else:
         for char in symbols:
             if char not in mail:
@@ -21,12 +21,19 @@ def check(line):
     return line
 
 
-with open('registrations_.txt', mode='r', encoding='utf-8') as ff:
+# TODO Открываем все три файла одним контекстным менеджером:
+with open('registrations_.txt', mode='r', encoding='utf-8') as ff, \
+    open('registration_bad.txt', mode='r', encoding='utf-8') as bad_log, \
+        open('registration_good.txt', mode='r', encoding='utf-8') as good_log:
     for line in ff:
         line = line[:-1]
         try:
             string = check(line)
         except NotNameError:
+            # TODO Так как обработка во всех исключений идентична, поэтому можно сделать обработку
+            #  одной веткой - укажите тут кортеж классов, а сообщение записывайте в лог так:
+            #  except (NotNameError, ...) as exc:
+            #      error_message = f'{i_line.rstrip()} - {exc.__class__.__name__} - {exc}\n'
             bad = open('registration_bad.log', mode='a', encoding='utf-8')
             bad.write(line + 'Имя содержит цифры' + '\n')
             bad.close()
